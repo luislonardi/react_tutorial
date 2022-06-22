419efcc7d2636699be8141c4534381d482982311
Luis Lonardi <luis.lonardi@licitacon.com.ar>
2022-06-21 21:33:42 -0300

    martes noche

--- src/index.js
+++ src/index.js
@@ -2,41 +2,39 @@ import React from "react";
 import ReactDom from 'react-dom';
 import './index.css';
 
-function BookList(){
+  const firstBook={
+    title: 'Pastillas para dormir',
+    author: 'Bayer',
+    img: 'https://m.media-amazon.com/images/I/81iMHrpqygL._AC_UL320_.jpg',
+  }
   
+  const secondBook={
+    title: 'Verity',
+    author: 'Kenneth',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71Mx-bE91+L._AC_UL254_SR254,254_.jpg',
+  }
+
+
+function BookList(){
   return(
   <section className= 'booklist'>
-  <Book />
-  <Book />
-  <Book />
-  <Book />
-  <Book />
-  <Book />
+  <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
+  <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}  />
   </section>
   )
 }
 
-const Book =()=>{
-  return (
-    <article className="book">
-      <Image></Image>
-      <Title />
-      <Author />
-    </article>
+const Book =(props)=>{
+  console.log(props)
+    return (
+  <article className="book">
+    <img src= {props.img} alt='figura alternativa'/>
+    <h1>{props.title}</h1>
+    <h4>{props.author}</h4>
+    
+  </article>
   )
 }
-
-const Image= ()=>(
-  <img
-    src='https://m.media-amazon.com/images/I/81iMHrpqygL._AC_UL320_.jpg'
-    alt='figura alternativa'
-  />  
-);
-
-const Title= ()=> <h1>Pastillas para dormir</h1>
-
-const Author= ()=> <h4> Bayer</h4>
-
 
 
 ReactDom.render(<BookList/>, document.getElementById('root'))
