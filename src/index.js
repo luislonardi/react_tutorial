3d419c33eaa5192fee3ee60ff7b08c1a0e7a4c01
Luis Lonardi <luis.lonardi@licitacon.com.ar>
2022-06-22 21:22:34 -0300

    miercoles noche

--- src/index.js
+++ src/index.js
@@ -4,37 +4,50 @@ import './index.css';
 
  const books=[
    {
+    id:1,
     title: 'Pastillas para dormir',
     author: 'Bayer',
-    imagen: 'https://m.media-amazon.com/images/I/81iMHrpqygL._AC_UL320_.jpg',
+    img: 'https://m.media-amazon.com/images/I/81iMHrpqygL._AC_UL320_.jpg',
   },
   {
+    id:2,
     title: 'Verity',
     author: 'Kenneth',
-    imagen: 'https://images-na.ssl-images-amazon.com/images/I/71Mx-bE91+L._AC_UL254_SR254,254_.jpg',
-  }
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71Mx-bE91+L._AC_UL254_SR254,254_.jpg',
+  },
+  {
+    id:3,
+    title: 'Sky is the limit',
+    author: 'John',
+    img: 'https://m.media-amazon.com/images/I/81yotbuQyfL._AC_UY218_.jpg',
+  },
 ];
-const names=['john','peter','susan']
-const newNames=names.map((name)=>{
-    return <h1>{name}</h1>
-})
+
 
 function BookList(){
   return(
   <section className= 'booklist'>
-    {newNames}
+    {books.map((book)=>{
+       return(
+        <Book key={book.id} {...book}></Book>
+      )
+    })}
   </section>
   )
 }
 
-const Book =(props)=>{
-  //const {img,title,author}=props
-  //console.log(props)
+const Book =({img,title,author})=>{
+  //attribute, eventHandler
+  //onClick, onMouseOver
+  const clickHandler=()=>{
+    alert('hello worlds')
+  }
     return (
   <article className="book">
-    <img src= {props.img} alt='figura alternativa'/>
-    <h1>{props.title}</h1>
-    <h4>{props.author}</h4>    
+    <img src= {img} alt='alt.'/>
+    <h1 onClick={()=>console.log(title)}>{title}</h1>
+    <h4>{author}</h4>
+    <button type='button' onClick={clickHandler}>Reference</button> 
   </article>
   )
 }
